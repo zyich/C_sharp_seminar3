22b5ce1238bee38ad227de13a8b02d3ab726b01c
zyich <zuilinho@yandex.ru>
2023-06-21 18:57:13 +0200

    task4

---
+++ task4(find_all_square_from1_to_N/Program.cs
@@ -0,0 +1,34 @@
+﻿/* Ярослав Безукладников: Напишите программу,
+ которая принимает на вход число (N) 
+ и выдаёт таблицу квадратов чисел
+от 1 до N.
+
+5 -> 1, 4, 9, 16, 25.
+2 -> 1,4
+*/
+
+int RandomNumber(int min, int max)
+{
+    int result = new Random().Next(min,max+1);
+
+    return result;
+
+}
+
+void Print(string text)
+{
+    Console.WriteLine(text);
+}
+
+void GetAllSquare(int rand )
+{
+    for (int i=1;i<=rand;i++)
+    {
+        Console.Write($"{Math.Pow(i,2)}, ");
+    }
+}
+
+int rand = RandomNumber(1,10);// между мин и макс
+Print($"{rand}");
+GetAllSquare(rand);
+
